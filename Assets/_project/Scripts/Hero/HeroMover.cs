86d15b9f24199dfebbca05c76ede3c978659125a
DimaDen94 <dmitrydlj@gmail.com>
2023-02-05 18:21:21 +0200

    removve jump and dynamic camera rotation

--- Assets/_project/Scripts/Hero/HeroMover.cs
+++ Assets/_project/Scripts/Hero/HeroMover.cs
@@ -9,24 +9,9 @@ public class HeroMover : MonoBehaviour
     [SerializeField] private float _topCameraClamp = 90.0f;
     [SerializeField] private float _bottomCameraClamp = -90.0f;
 
-    [Space(10)]
-    [SerializeField] private float _jumpHeight = 1.2f;
-    [SerializeField] private float _gravity = -15.0f;
-    [SerializeField] private float _groundedRadius = 0.5f;
-    [SerializeField] private float _groundedOffset = -0.14f;
-    [SerializeField] private LayerMask GroundLayers;
-    [SerializeField] private float JumpTimeout = 0.1f;
-    [SerializeField] private float FallTimeout = 0.15f;
-
-    public bool _grounded = true;
 
     private float _cinemachineTargetPitch;
     private float _rotationVelocity;
-
-    private float _verticalVelocity;
-    private float _jumpTimeoutDelta;
-    private float _fallTimeoutDelta;
-    private float _terminalVelocity = 53.0f;
     
     private IInputService _inputService;
     private Camera _camera;
@@ -38,8 +23,6 @@ public class HeroMover : MonoBehaviour
 
     private void Update()
     {
-        JumpAndGravity();
-        GroundedCheck();
         Move();
     }
     private void LateUpdate()
@@ -60,7 +43,7 @@ public class HeroMover : MonoBehaviour
             movementVector = transform.right * _inputService.MoveAxis.x + transform.forward * _inputService.MoveAxis.y;
 
         movementVector += Physics.gravity;
-        _characterController.Move(_movementSpeed * movementVector * Time.deltaTime + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+        _characterController.Move(_movementSpeed * movementVector * Time.deltaTime);
     }
 
     private void CameraRotation()
@@ -84,49 +67,5 @@ public class HeroMover : MonoBehaviour
         if (lfAngle > 360f) lfAngle -= 360f;
         return Mathf.Clamp(lfAngle, lfMin, lfMax);
     }
-
-
-    private void GroundedCheck()
-    {
-        // set sphere position, with offset
-        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - _groundedOffset, transform.position.z);
-        _grounded = Physics.CheckSphere(spherePosition, _groundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
-    }
-    private void JumpAndGravity()
-    {
-        if (_grounded)
-        {
-            _fallTimeoutDelta = FallTimeout;
-            if (_verticalVelocity < 0.0f)
-            {
-                _verticalVelocity = -2f;
-            }
-            Debug.Log(_inputService.IsJumpButton());
-            if (_inputService.IsJumpButton() && _jumpTimeoutDelta <= 0.0f)
-            {
-                _verticalVelocity = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
-            }
-
-            if (_jumpTimeoutDelta >= 0.0f)
-            {
-                _jumpTimeoutDelta -= Time.deltaTime;
-            }
-        }
-        else
-        {
-            _jumpTimeoutDelta = JumpTimeout;
-
-            if (_fallTimeoutDelta >= 0.0f)
-            {
-                _fallTimeoutDelta -= Time.deltaTime;
-            }
-
-            //_input.jump = false;
-        }
-
-        if (_verticalVelocity < _terminalVelocity)
-        {
-            _verticalVelocity += _gravity * Time.deltaTime;
-        }
-    }
+    
 }
